3869daf9251b8282a8d2e1d363e08fad154700c0
ducxomme <ducxomme@gmail.com>
2022-12-24 19:31:06 +0700

    [update] model ide_helper generate

--- system/server/app/Models/Agency.php
+++ system/server/app/Models/Agency.php
@@ -5,6 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Agency
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Agency newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Agency newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Agency query()
+ * @mixin \Eloquent
+ */
 class Agency extends Model
 {
     use HasFactory;
--- system/server/app/Models/Order.php
+++ system/server/app/Models/Order.php
@@ -5,6 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Order
+ *
+ * @property int $id
+ * @property int $customer_id
+ * @property string $order_date
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Order newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Order newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Order query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereCustomerId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereOrderDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Order extends Model
 {
     use HasFactory;
