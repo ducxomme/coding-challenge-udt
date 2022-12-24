474d3745cf826989075701a7d364e07bb0c3f359
ducxomme <ducxomme@gmail.com>
2022-12-24 19:44:10 +0700

    [add] agency's resources all APIs implemented

--- system/server/app/Http/Controllers/API/V1/AgencyController.php
+++ system/server/app/Http/Controllers/API/V1/AgencyController.php
@@ -1,11 +1,115 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\Agency;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Mockery\Exception;
 
-class AgencyController extends Controller
+final class AgencyController extends Controller
 {
-    //
+    /**
+     * @return JsonResponse
+     */
+    public function list(): JsonResponse
+    {
+        $agencies = Agency::all();
+
+        $result = [];
+
+        foreach ($agencies as $agency) {
+            $result[] = [
+                'id' => $agency['id'],
+                'name' => $agency['name'],
+                'address' => $agency['address'],
+                'phoneNumber' => $agency['phone_number'],
+            ];
+        }
+
+        return new JsonResponse( [
+            'data' => $result,
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function get(Request $request): JsonResponse
+    {
+        $agency = Agency::find((int) $request->route('id'));
+
+        $result = [];
+
+        if (!is_null($agency)) {
+            $result = [
+                'id' => $agency->id,
+                'name' => $agency->name,
+                'address' => $agency->address,
+                'phoneNumber' => $agency->phone_number,
+            ];
+        }
+
+        return new JsonResponse( [
+            'data' => $result,
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return void
+     */
+    public function create(Request $request): void
+    {
+        /*
+        {
+          "data": {
+            "name": "XXX",
+            "address": "Address",
+            "phoneNumber": "0989999888"
+          }
+        }
+        */
+        $requestBody = $request->get('data');
+
+        $agency = new Agency();
+        $agency->name = $requestBody['name'];
+        $agency->address = $requestBody['address'];
+        $agency->phone_number = $requestBody['phoneNumber'];
+
+        $agency->save();
+    }
+
+    /**
+     * @param Request $request
+     * @return void
+     */
+    public function update(Request $request): void
+    {
+        $requestBody = $request->get('data');
+
+        $agency = Agency::find((int) $request->route('id'));
+
+        if (is_null($agency)) {
+            throw new Exception('Invalid parameter');
+        }
+
+        $agency->name = $requestBody['name'];
+        $agency->address = $requestBody['address'];
+        $agency->phone_number = $requestBody['phoneNumber'];
+
+        $agency->update();
+    }
+
+    /**
+     * @param Request $request
+     * @return void
+     */
+    public function delete(Request $request): void
+    {
+        Agency::where('id', (int) $request->route('id'))->delete();
+    }
 }
--- system/server/app/Models/Agency.php
+++ system/server/app/Models/Agency.php
@@ -8,12 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * App\Models\Agency
  *
+ * @property int $id
+ * @property string $name
+ * @property string $address
+ * @property string $phone_number
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
  * @method static \Illuminate\Database\Eloquent\Builder|Agency newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Agency newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Agency query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Agency whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Agency whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Agency whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Agency whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Agency wherePhoneNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Agency whereUpdatedAt($value)
  * @mixin \Eloquent
  */
 class Agency extends Model
 {
     use HasFactory;
+
+    protected $table = 'agency';
 }
