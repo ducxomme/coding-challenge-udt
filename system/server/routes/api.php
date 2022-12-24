d2486674b409e2cec39a56bed4590ffdb5e07198
ducxomme <ducxomme@gmail.com>
2022-12-24 17:36:32 +0700

    [add] define routes path

--- system/server/routes/api.php
+++ system/server/routes/api.php
@@ -14,12 +14,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
 
+// Customer APIs
+Route::get('/v1/customers', [\App\Http\Controllers\Api\V1\CustomerController::class, 'list']);
+Route::get('/v1/customers/{id}', [\App\Http\Controllers\Api\V1\CustomerController::class, 'get']);
+Route::post('/v1/customers', [\App\Http\Controllers\Api\V1\CustomerController::class, 'create']);
+Route::put('/v1/customers/{id}', [\App\Http\Controllers\Api\V1\CustomerController::class, 'update']);
+Route::delete('/v1/customers/{id}', [\App\Http\Controllers\Api\V1\CustomerController::class, 'delete']);
 
-Route::get('/v1/test', function() {
-    return 'Hello World';
-});
+// Agency APIs
+Route::get('/v1/agencies', [\App\Http\Controllers\Api\V1\AgencyController::class, 'list']);
+Route::get('/v1/agencies/{id}', [\App\Http\Controllers\Api\V1\AgencyController::class, 'get']);
+Route::post('/v1/agencies', [\App\Http\Controllers\Api\V1\AgencyController::class, 'create']);
+Route::put('/v1/agencies/{id}', [\App\Http\Controllers\Api\V1\AgencyController::class, 'update']);
+Route::delete('/v1/agencies/{id}', [\App\Http\Controllers\Api\V1\AgencyController::class, 'delete']);
+
+// Product APIs
+// Product Category
+Route::get('/v1/categories', [\App\Http\Controllers\Api\V1\CategoryController::class, 'list']);
+Route::get('/v1/categories/{id}', [\App\Http\Controllers\Api\V1\CategoryController::class, 'get']);
+Route::post('/v1/categories', [\App\Http\Controllers\Api\V1\CategoryController::class, 'create']);
+Route::put('/v1/categories/{id}', [\App\Http\Controllers\Api\V1\CategoryController::class, 'update']);
+Route::delete('/v1/categories/{id}', [\App\Http\Controllers\Api\V1\CategoryController::class, 'delete']);
+
+// Product
+Route::get('/v1/products', [\App\Http\Controllers\Api\V1\ProductController::class, 'list']);
+Route::get('/v1/products/{id}', [\App\Http\Controllers\Api\V1\ProductController::class, 'get']);
+Route::post('/v1/products', [\App\Http\Controllers\Api\V1\ProductController::class, 'create']);
+Route::put('/v1/products/{id}', [\App\Http\Controllers\Api\V1\ProductController::class, 'update']);
+Route::delete('/v1/products/{id}', [\App\Http\Controllers\Api\V1\ProductController::class, 'delete']);
+
+// Order
+Route::get('/v1/orders', [\App\Http\Controllers\Api\V1\OrderController::class, 'list']);
+Route::get('/v1/orders/{id}', [\App\Http\Controllers\Api\V1\OrderController::class, 'get']);
+Route::post('/v1/orders', [\App\Http\Controllers\Api\V1\OrderController::class, 'create']);
+Route::put('/v1/orders/{id}', [\App\Http\Controllers\Api\V1\OrderController::class, 'update']);
+Route::delete('/v1/orders/{id}', [\App\Http\Controllers\Api\V1\OrderController::class, 'delete']);
